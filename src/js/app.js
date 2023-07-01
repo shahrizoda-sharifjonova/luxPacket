49b1c0de815a6d8b5d852e65d596d3e90abec5ec
shahriyor-sharifjonov <shahrizodadeveloping@gmail.com>
2023-07-01 10:11:39 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,31 @@ import * as functions from "./modules/functions.js";
 
 functions.isWebp();
 
-// import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, {Pagination } from 'swiper';
 
-// const swiper = new Swiper();
+const swiper = new Swiper();
+
+new Swiper(".intro__swiper", {
+    grabCursor: true,
+    modules: [Pagination],
+    slidesPerView: 1,
+    spaceBetween:0,
+    pagination: {
+      el: ".intro__pagination",
+      clickable: true,
+    },
+    breakpoints:{
+      992:{
+        spaceBetween:30,
+      }
+    }
+});
+const menu = document.querySelector('.menu');
+const headerList = document.querySelector('.header__list');
+const body = document.querySelector('body');
+
+menu.addEventListener('click', (e)=>{
+    menu.classList.toggle('active')
+    headerList.classList.toggle('active')
+    body.classList.toggle('hidden')
+})
